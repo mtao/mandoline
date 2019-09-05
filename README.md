2312fc30fdacc38f714c6f5236cc03aabc209d12
Michael Tao <mtao@dgp.toronto.edu>
2019-09-04 23:27:07 -0400

    random updates

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# mandoline
+# Mandoline
+
 A robust cut-cell mesh generator for arbitrary triangle-mesh inputs
+
+## Dependencies
+### Compilation
+Mandoline depends on a C++17 enabled 
+
+### Built-in Libraries
+Mandoline depends on Michael Tao's messy ``[core](https://github.com/mtao/core)'' library, [libigl](https://github.com/libigl/libigl).
+
+-core and libigl are included as submodules, and can be added by:
+```bash
+git submodule update --init --recursive
+```
+
+### Required External Libraries
+- A relatively new verison of [Eigen3](https://eigen.tuxfamily.org) is required (3.3.7 or the development branch?). The version provided by libigl will not suffice.
+- [Boost](https://boost.org) is required for threads.
+- [OpenMP](https://www.openmp.org) is required.
+
+In order to handle self-intersections Mandoline requires 
+- [CGAL](https://www.cgal.org)
+- [gmp](https://gmplib.org)
+- [mpfr](https://www.mpfr.org).
+
+### Optional Libraries
+Magnum depends on [Magnum](https://github.com/mosra) and [Corrade](https://github.com/mosra/corrade) for visualization.
+If Magnum and Corrade are available, targets that depend on visualization can be enabled by ```-DUSE_OPENGL ON``` when running cmake.
--- src/construction/remesh_self_intersections.cpp
+++ src/construction/remesh_self_intersections.cpp
@@ -1,13 +1,16 @@
 #include "mandoline/construction/remesh_self_intersections.hpp"
+#ifdef MANDOLINE_HANDLE_SELF_INTERSECTIONS
 #define __gmp_const const
 #include <igl/copyleft/cgal/remesh_self_intersections.h>
 #include <igl/remove_unreferenced.h>
+#endif
 
 #include <mtao/logging/profiler.hpp>
 
 namespace mandoline::construction {
     std::tuple<mtao::ColVecs3d,mtao::ColVecs3i> remesh_self_intersections(const mtao::ColVecs3d& V,const mtao::ColVecs3i& F) {
         auto t = mtao::logging::profiler("Remeshing self intersections");
+#ifdef MANDOLINE_HANDLE_SELF_INTERSECTIONS
         Eigen::MatrixXd VV,SV;
         Eigen::MatrixXi FF,SF,IF;
         Eigen::VectorXi J,IM,UIM;
@@ -16,5 +19,9 @@ namespace mandoline::construction {
         std::for_each(FF.data(),FF.data()+FF.size(),[&IM](int & a){a=IM(a);});
         igl::remove_unreferenced(VV,FF,SV,SF,UIM);
         return std::make_tuple(mtao::ColVecs3d(SV.transpose()),mtao::ColVecs3i(SF.transpose()));
+#else
+        mtao::logging::error() << "mandoline::construction::remesh_self_intersections was called even though it was built without the ability to handle such things! Try building with ``-DHANDLE_SELF_INTERSECTIONS";
+#endif//MANDOLINE_HANDLE_SELF_INTERSECTIONS
+        return {V,F};
     }
 }
