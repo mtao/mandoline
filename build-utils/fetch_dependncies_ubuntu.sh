4480423d6208a52d0f0e5ddc298f82dfc0c943c8
Michael Tao <michael@augmenta.ai>
2019-10-24 15:47:02 -0400

    dep update

--- build-utils/fetch_dependncies_ubuntu.sh
+++ build-utils/fetch_dependncies_ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install -y git cmake libboost-thread-dev libmpfr-dev libmpfrc++-dev libcgal-dev libeigen3-dev protobuf-compiler mercurial
+sudo apt install -y git cmake libboost-thread-dev libmpfr-dev libmpfrc++-dev libcgal-dev libeigen3-dev protobuf-compiler mercurial debhelper libgl-dev libopenal-dev libglfw3-dev libsdl2-dev libbullet-dev libglm-dev
 
 git submodule update --recursive --init
 
