85ba5636a6420769d8837efefe597784aafdf8e6
Michael Tao <mtao@dgp.toronto.edu>
2020-02-19 14:55:28 -0500

    trying something out remotely

--- src/vertex.cpp
+++ src/vertex.cpp
@@ -1,5 +1,5 @@
 #include "mandoline/vertex.hpp"
 
-#if defined(_MSC_VER)
+#if defined(_MSC_VER) || defined(__APPLE__)
 const static double threshold_epsilon = std::sqrt(std::numeric_limits<double>::epsilon());
 #endif
