1a6150de8f06120765eb7059ed5f912959832bae
Michael Tao <mtao@dgp.toronto.edu>
2020-02-19 15:01:24 -0500

    removed another minoir threshold thing [ci skip]

--- src/vertex.cpp
+++ src/vertex.cpp
@@ -1,5 +1,5 @@
 #include "mandoline/vertex.hpp"
 
-#if defined(_MSC_VER) || defined(__APPLE__) || defined(__TEST__)
+#if defined(_MSC_VER) || defined(__APPLE__) || defined(__STATIC_TEST__)
 const double threshold_epsilon = std::sqrt(std::numeric_limits<double>::epsilon());
 #endif
