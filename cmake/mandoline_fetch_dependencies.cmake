7ae11b0cfd8c5022cf1a9dcf12961acaa2e0d57a
Michael Tao <mtao@dgp.toronto.edu>
2020-04-18 12:54:12 -0400

    mtao bump

--- cmake/mandoline_fetch_dependencies.cmake
+++ cmake/mandoline_fetch_dependencies.cmake
@@ -2,7 +2,7 @@ include(FetchContent REQUIRED)
 
 
 set(MTAO_COMMIT 
-    c60fac422ae9e762494b7eb13c744d509331fe4b
+    733eeaa5c61be8edd7a479fccaaa5fe77bb9593d
     )
 set(PROTOBUF_COMMIT v3.11.3)
 set(LIBIGL_COMMIT v2.1.0)
