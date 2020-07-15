431c708273e3c63502268eb3198cc87ca215cb2d
Michael Tao <mtao@dgp.toronto.edu>
2020-07-15 11:33:22 -0400

    version bump

--- cmake/mandoline_fetch_dependencies.cmake
+++ cmake/mandoline_fetch_dependencies.cmake
@@ -2,7 +2,7 @@ include(FetchContent REQUIRED)
 
 
 set(MTAO_COMMIT 
-    07b0c81bfdfcbb8320fd0b21713cc2a6161f0b70
+    ec2fb90886f10c1dae6c1c517a1fc3900d3e6523
     )
 set(PROTOBUF_COMMIT v3.11.3)
 set(LIBIGL_COMMIT v2.1.0)
